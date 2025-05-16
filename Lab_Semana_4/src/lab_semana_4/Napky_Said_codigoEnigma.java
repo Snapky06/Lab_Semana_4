a97b2d58a5980439d4f6b0863a36318363231fca
Snapky06 <saidnapky@gmail.com>
2025-05-16 09:41:17 -0600

    Avance del tercero

--- Lab_Semana_4/src/lab_semana_4/Napky_Said_codigoEnigma.java
+++ Lab_Semana_4/src/lab_semana_4/Napky_Said_codigoEnigma.java
@@ -4,10 +4,50 @@
  */
 package lab_semana_4;
 
+import static java.lang.System.out;
+import java.util.Scanner;
+
 /**
  *
  * @author saidn
  */
 public class Napky_Said_codigoEnigma {
-    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        int opcion=0;
+        
+       
+        String par= "";
+        String impar="";
+        int numero_par=0;
+        int numero_impar=0;
+        
+        while(opcion!=3){
+        
+             System.out.println("Ingrese Una Palabra");
+             String palabra = leer.next();
+        
+            System.out.println("[1]-Encriptar Texto");
+            System.out.println("[2]-Desencriptar Texto");
+            System.out.println("[3]-Regresar");
+            
+            switch(opcion){
+            
+                case 1:
+                    for(int indice=0;indice<palabra.length();indice++){
+                        numero_par+=0;
+                    par+=palabra.charAt(numero_par);
+                    numero_par+=2;
+                    
+                    if(indice!=numero_par){
+                    impar+=palabra.charAt(indice);
+                    }
+                    
+                    }
+                    break;
+            
+            }
+        
+        }
+    }
 }
