738b1f9a526365c61fbb61b773c2edcde2cacc4a
Snapky06 <saidnapky@gmail.com>
2025-05-16 08:53:10 -0600

    Subida De Proyecto

---
+++ Lab_Semana_4/src/lab_semana_4/Napky_Said_Filtrar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_semana_4;
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+/**
+ *
+ * @author saidn
+ */
+public class Napky_Said_Filtrar {
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+                
+        System.out.println("Ingrese Una Frase : ");
+    }
+}
