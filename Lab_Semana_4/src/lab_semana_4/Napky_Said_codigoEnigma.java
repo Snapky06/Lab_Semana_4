7648212c2e27ccf3bf11bbd2ff20fee8e978bcf2
Snapky06 <saidnapky@gmail.com>
2025-05-16 10:04:46 -0600

    Termine el ultimo codigo
    -said

--- Lab_Semana_4/src/lab_semana_4/Napky_Said_codigoEnigma.java
+++ Lab_Semana_4/src/lab_semana_4/Napky_Said_codigoEnigma.java
@@ -12,42 +12,77 @@ import java.util.Scanner;
  * @author saidn
  */
 public class Napky_Said_codigoEnigma {
+
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in).useDelimiter("\n");
-        int opcion=0;
-        
-       
-        String par= "";
-        String impar="";
-        int numero_par=0;
-        int numero_impar=0;
-        
-        while(opcion!=3){
-        
-             System.out.println("Ingrese Una Palabra");
-             String palabra = leer.next();
-        
+        int opcion = 0;
+
+        String palabra = "";
+        String par = "";
+        String impar = "";
+        int numero_par = 0;
+        int numero_impar = 0;
+        String palabras_juntas = "";
+        String palabra_og = "";
+
+        while (opcion != 3) {
+
             System.out.println("[1]-Encriptar Texto");
             System.out.println("[2]-Desencriptar Texto");
             System.out.println("[3]-Regresar");
-            
-            switch(opcion){
-            
+            System.out.print("Que Opcion Desea (1,2 o 3) : ");
+
+            opcion = leer.nextInt();
+
+            switch (opcion) {
+
                 case 1:
-                    for(int indice=0;indice<palabra.length();indice++){
-                        numero_par+=0;
-                    par+=palabra.charAt(numero_par);
-                    numero_par+=2;
-                    
-                    if(indice!=numero_par){
-                    impar+=palabra.charAt(indice);
+
+                    System.out.print("Ingrese Una Palabra : ");
+                    palabra = leer.next();
+
+                    for (int indice = 0; indice < palabra.length(); indice++) {
+                        if (indice % 2 == 0) {
+                            par += palabra.charAt(indice);
+                        } else {
+                            impar += palabra.charAt(indice);
+                        }
                     }
-                    
-                    }
+
+                    palabras_juntas = par + impar;
+                    System.out.println(palabras_juntas);
+                    par = "";
+                    impar = "";
                     break;
-            
+
+                case 2:
+                    System.out.print("Ingrese Una Palabra : ");
+                    palabra = leer.next();
+                    int mitad = (palabra.length()+1)/2;
+                    palabra_og="";
+                    
+                    int pospar=0;
+                    int posimpar=mitad;
+                    
+                    for (int indice = 0; indice < palabra.length(); indice++) {
+                        if (indice % 2 == 0&&pospar<mitad) {
+                            palabra_og+=palabra.charAt(pospar);
+                            pospar++;
+                        } else {
+                            palabra_og+=palabra.charAt(posimpar);
+                            posimpar++;
+                        }
+                    }
+
+                    System.out.println(palabra_og);
+
+                    break;
+
+                default:
+                    System.out.println("Esta Opcion No Existe");
+                    break;
             }
-        
+
         }
     }
 }
